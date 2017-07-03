a6bcdc0bdf2b4e43c64c6367520ca709039b4cc1
Kevin <kevin.troutt@gmail.com>
2017-07-02 20:57:00 -0700

    Finished hangman

---
+++ Hangman/hangman.py
@@ -0,0 +1,57 @@
+import random
+
+def get_selected():
+	with open ("words.txt", "r") as f:
+		lines = f.readlines()
+	for index in range(len(lines)):
+		lines[index] = lines[index].strip()
+	# lines = [s.strip() for s in lines]
+	# print (lines[:10])
+	words =[]
+	"""for index in range(len(lines)):
+		if len(lines[index]) >=4:
+			words.append(lines[index]) """
+	for w in lines:
+		if len(w) >=4:
+			words.append(w)
+
+	selected = random.choice(words)
+	return selected
+
+selected = get_selected()
+
+def update(selected, guesses, lives):
+	guessed = ""
+	for i in selected:
+		if i in guesses:
+			guessed += i
+		else:
+			guessed += "_"
+	print("\nWord so far:",guessed)
+	print("All guesses:", guesses)
+	print("Current lives:", lives)
+
+def finished(selected, guesses):
+	for i in selected:
+		if i not in guesses:
+			return False
+	return True
+
+lives = 6
+guesses = []
+while lives > 0 and not finished(selected, guesses):
+	update(selected, guesses, lives)
+	guess = input("Enter a letter:")[0]
+	if guess not in selected and guess not in guesses:
+		lives -= 1
+	if guess in guesses:
+		print("You've already entered that")
+	guesses.append(guess)
+
+if lives > 0:
+	print("Congrats!")
+else:
+	print("You lose!")
+
+
+print (selected)
